40fb525a033d70bc5a832867039b995dd3976753
Bernieson Sahagun <berniesonsahagun@gmail.com>
2025-01-11 10:23:40 +0800

    feat: updated portfolio projects

--- src/components/modals/PortfolioModal.tsx
+++ src/components/modals/PortfolioModal.tsx
@@ -26,29 +26,49 @@ export function PortfolioModal({ handleCloseModal }: ModalProps) {
           are interested in hitting me up for a job, just contact me!
         </p>
 
-        <h2>Internal Automation Tools for Website Maintenance</h2>
-        <h3>Visual Integrity Checker</h3>
+        <h2>SaaS Applications</h2>
+
+        <h3>Togather</h3>
         <span className="role">Role: Software Engineer</span>
-        <h4>Background</h4>
+
+        <h4>Description</h4>
         <p>
-          Managing multiple websites across platforms like WordPress, Webflow,
-          Wix, and static sites can be a nightmare, especially when caches fail
-          to update properly, causing broken styles. This experience inspired me
-          to develop a tool that simplifies monitoring and maintenance, ensuring
-          websites remain stable and visually intact.
+          Togather is a leading event booking platform in the UK. My role in
+          this project was to create reusable components exactly as the design
+          provided. Moreover, I integrated these components with their backend,
+          ensuring performance and functionality is up to the coding standards.
         </p>
 
-        <h4>Solution</h4>
         <p>
-          Using Playwright and some CRON Job, I built a script that would take
-          screenshots of the websites periodically. These screenshots would be
-          compared to a baseline screenshot. If a significant change was
-          detected, a report would be sent to me indicating a problem.
+          View the website here:{" "}
+          <a href="https://togather.com/" target="_blank">
+            https://togather.com/
+          </a>
         </p>
 
         <hr />
 
-        <h2>SaaS App</h2>
+        <h3>Killington Ski Resort (Mono-repo Multi-website Deploy)</h3>
+        <span className="role">Role: Software Engineer</span>
+
+        <h4>Description</h4>
+        <p>
+          Killington Ski Resort is a Mono-repo Multi-website Deployment using
+          Gatsby and GraphQL. My role was to convert an existing Angular
+          codebase repository into the new Gatsby and GraphQL setup. There were
+          10+ websites within the same repository. I also maintained and managed
+          the components afterwards, ensuring that the transition was seamlesss.
+        </p>
+
+        <p>
+          View the website here:{" "}
+          <a href="https://www.killington.com/" target="_blank">
+            https://www.killington.com/
+          </a>
+        </p>
+
+        <hr />
+
         <h3>Merk</h3>
         <span className="role">Role: Hobbyist</span>
         <h4>Background</h4>
@@ -69,6 +89,28 @@ export function PortfolioModal({ handleCloseModal }: ModalProps) {
           frontend, paired with .NET Framework for a code-first backend
           approach. This project not only delivers a scalable platform but also
           helps me enhance my skills in both frontend and backend development.
+        </p>
+
+        <hr />
+
+        <h2>Internal Automation Tools for Website Maintenance</h2>
+        <h3>Visual Integrity Checker</h3>
+        <span className="role">Role: Software Engineer</span>
+        <h4>Background</h4>
+        <p>
+          Managing multiple websites across platforms like WordPress, Webflow,
+          Wix, and static sites can be a nightmare, especially when caches fail
+          to update properly, causing broken styles. This experience inspired me
+          to develop a tool that simplifies monitoring and maintenance, ensuring
+          websites remain stable and visually intact.
+        </p>
+
+        <h4>Solution</h4>
+        <p>
+          Using Playwright and some CRON Job, I built a script that would take
+          screenshots of the websites periodically. These screenshots would be
+          compared to a baseline screenshot. If a significant change was
+          detected, a report would be sent to me indicating a problem.
         </p>
 
         <hr />
